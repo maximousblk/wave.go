82bfe820b7873a427e4d2155932a5d82a72de7f6
Maximous Black <maximousblk@gmail.com>
2021-05-09 10:42:32 +0000

    feat: write keyfiles to file

--- wave.go
+++ wave.go
@@ -1,34 +1,38 @@
 package main
 
 import (
-	// standard
+	// Standard
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"math/big"
+	"os"
+	"path/filepath"
 	"regexp"
 
-	// third party
+	// Third Party
 	"github.com/alexflint/go-arg"
 	"github.com/mendsley/gojwk"
 )
 
-// goreleaser variables
+// GoReleaser
 var (
 	version = "dev"
 	commit  = "untagged"
 )
 
-// commandline
+// go-arg
 type args struct {
 	Workers int    `arg:"-w,--workers" default:"4" help:"Number of workers to spawn"`
 	Count   int    `arg:"-n,--number" default:"1" help:"Number of wallets to generate"`
+	Output  string `arg:"-o,--output" default:"./keyfiles" help:"Output directory"`
 	Pattern string `arg:"positional,required" help:"Regex pattern to match the wallet address"`
 }
 
-// set version and commit
+// set go-arg version and commit from GoReleaser
 func (args) Version() string {
 	return fmt.Sprintf("wave %v (%v)", version, commit[:8])
 }
@@ -38,43 +42,55 @@ func main() {
 	var args args
 	arg.MustParse(&args)
 
-	numWorkers := args.Workers          // number of workers to spawn
-	numWallets := args.Count            // number of wallets to generate
-	vanityPattern := args.Pattern       // regex pattern to match the wallet address
-	walletChan := make(chan *Wallet, 1) // channel to get wallets from workers
+	numWorkers := args.Workers           // Number of workers to spawn
+	numWallets := args.Count             // Number of wallets to generate
+	vanityPattern := args.Pattern        // Regex pattern to match the wallet address
+	outDir := filepath.Join(args.Output) // Output directory
+	walletChan := make(chan Wallet, 1)   // Channel to get wallets from workers
 
-	fmt.Println("Workers spawned:", numWorkers)
-	fmt.Println("Wallets to generate:", numWallets)
-	fmt.Println("Wallet pattern:", "/"+vanityPattern+"/")
+	fmt.Println("Pattern:", "/"+vanityPattern+"/")
+	fmt.Println("Outputs:", outDir)
+	fmt.Println("Workers:", numWorkers)
+	fmt.Println("Wallets:", numWallets)
 
 	for n := 1; n <= numWallets; n++ {
+		// spawn workers
 		for w := 1; w <= numWorkers; w++ {
-			// spawn a worker
 			go worker(w, vanityPattern, walletChan)
 		}
 
 		// get wallet from worker
 		k := <-walletChan
 
-		fmt.Println("found!:", k.address)
-		keyfile, err := gojwk.Marshal(k.key) // get keyfile as json string
+		fmt.Println("[MATCH] address:", k.address)
 
+		// get keyfile as json byte slice
+		keyfile, err := gojwk.Marshal(k.key)
 		errcheck(err)
 
-		// TODO(@maximousblk): add logic to output to a file
-		fmt.Println("keyfile:", string(keyfile))
+		keyfilePath := filepath.Join(outDir, "arweave-keyfile-"+k.address+".json")
+
+		// Check if output directory exists
+		if _, err := os.Stat(outDir); os.IsNotExist(err) {
+			// If not, create it
+			derr := os.Mkdir(outDir, 0777)
+			errcheck(derr)
+		}
+
+		// Write keyfile to file
+		ioutil.WriteFile(keyfilePath, keyfile, 0666)
+		fmt.Println("[EMIT] keyfile:", keyfilePath)
 	}
 }
 
-func worker(workerId int, pattern string, walletChan chan<- *Wallet) {
+func worker(workerId int, pattern string, walletChan chan<- Wallet) {
 	for {
-		// generate wallet
+		// Generate wallet
 		wallet := GenerateWallet()
 		walletAddress := wallet.address
 
 		// check if wallet address matches the provided pattern
 		match, err := regexp.MatchString(pattern, walletAddress)
-
 		errcheck(err)
 
 		fmt.Printf("[WORKER%v] address: %v | match: %v]\n", workerId, walletAddress, match)
@@ -92,22 +108,21 @@ type Wallet struct {
 	key     *gojwk.Key
 }
 
-func GenerateWallet() *Wallet {
+func GenerateWallet() Wallet {
 	// generate an RSA key
 	reader := rand.Reader
 	rsaKey, err := rsa.GenerateKey(reader, 4096)
-
 	errcheck(err)
 
 	// create new wallet instance
-	wallet := &Wallet{}
+	wallet := Wallet{}
 
 	// Generate keyfile from RSA key
 	wallet.key = &gojwk.Key{
-		Kty: "RSA",
-		E:   base64.RawURLEncoding.EncodeToString(big.NewInt(int64(rsaKey.E)).Bytes()),
-		N:   base64.RawURLEncoding.EncodeToString(rsaKey.N.Bytes()),
-		D:   base64.RawURLEncoding.EncodeToString(rsaKey.D.Bytes()),
+		Kty: "RSA",                                                                     // Key type
+		N:   base64.RawURLEncoding.EncodeToString(rsaKey.N.Bytes()),                    // Modulus for the public key and the private keys
+		D:   base64.RawURLEncoding.EncodeToString(rsaKey.D.Bytes()),                    // Private key exponent
+		E:   base64.RawURLEncoding.EncodeToString(big.NewInt(int64(rsaKey.E)).Bytes()), // public key exponent
 	}
 
 	// Generate wallet address
